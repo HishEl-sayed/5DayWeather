30fbbf00da40ee258708365be3062bcbcca0bf43
Hisham El-sayed <h.el-sayed@amaze.com>
2016-08-11 16:20:58 +0100

    modified the promise syntax to now write to the object then use the append function

--- js/fetch.js
+++ js/fetch.js
@@ -1,4 +1,14 @@
+'use strict';
 
+
+(function() {
+
+    getData('http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=07c942ac99d05229807385fde0045886') //grab the 
+        .then(processWeatherData(weather))
+        .then(appendData())
+        .catch(error);
+
+})();
 
 function getData(url) {
 
@@ -28,81 +38,86 @@ function getData(url) {
     });
 }
 
+// Data parsing
 function processWeatherData(weather) {
-   
-    return function(data) { //begin function to start reading the data promised back
-        console.log(data);
 
-        var i, item, weatherData, weatherTxt = ''; 
-        var city = data.name;
-        var weather = data.weather;
-        var returnData = [];
-        //set the weather returned as a variable
-
-        for (i=0; i < weather.length; i++) {
-
-            var returnItem = { //create the return object that I'm going to append to the DOM
-                temperatureData: null,
-                city: city,
-                forecast: null
-            };
-
-            //weatherTxt += '<div class="weather">'; // create string that will hold data returned
-            //iterate through the returned data 
-            var weatherData = weather[i];
-
-            Object.keys(weatherData).forEach(function(key) { //look through weather object 
-
-                var forecast = (key + ': ' + weatherData[key]);
-                // weatherTxt += '<div>' + forecast + '</div>';
-                returnItem.forecast = forecast;
-
-            });
-
-            var temperature = data.main; // define temperature, specified as 'main' within the AJAX call
-
-            Object.keys(temperature).forEach(function(key) { //iterate through the length of the object. Prototype allows us to ignore prototype objects
-                    
-                var temperatureData = (key, temperature[key]);
-                //weatherTxt += '<div>' + temperatureData + '</div>';
-                returnItem.temperatureData = temperatureData;
-
-            });
-
-            //weatherTxt += '</div>';
-
-            returnData.push(returnItem);
-
-            document.getElementById('weather').innerHTML = weatherTxt;
-
-
+    return function(weatherData) {
+        console.log(weatherData)
+        var returnData = {
+            name: weatherData.name,
+            temperature: weatherData.main,
+            weather: weatherData.weather
         }
-        console.log(city);
-
-        //return returnData;
-        appendData(returnData);
-    }
-
-
+        return returnData;
+   };
 }
+
+// function _processWeatherData(weather) {
+   
+//     return function(data) { //begin function to start reading the data promised back
+//         console.log(data);
+//         console.log(data.main.humidity);
+
+//         var i, item, weatherData, weatherTxt = ''; 
+//         var city = data.name;
+//         var weather = data.weather;
+//         var returnData = [];
+//         //set the weather returned as a variable
+
+//         return;
+
+//         for (i=0; i < weather.length; i++) {
+
+//             var returnItem = { //create the return object that I'm going to append to the DOM
+//                 temperatureData: null,
+//                 city: city,
+//                 forecast: null
+//             };
+
+//             //weatherTxt += '<div class="weather">'; // create string that will hold data returned
+//             //iterate through the returned data 
+//             var weatherData = weather[i];
+
+//             Object.keys(weatherData).forEach(function(key) { //look through weather object 
+
+//                 var forecast = (key + ': ' + weatherData[key]);
+//                 // weatherTxt += '<div>' + forecast + '</div>';
+//                 returnItem.forecast = forecast;
+
+//             });
+
+//             var temperature = data.main; // define temperature, specified as 'main' within the AJAX call
+
+//             Object.keys(temperature).forEach(function(key) { //iterate through the length of the object. Prototype allows us to ignore prototype objects
+                    
+//                 var temperatureData = (key, temperature[key]);
+//                 //weatherTxt += '<div>' + temperatureData + '</div>';
+//                 returnItem.temperatureData = temperatureData;
+//                 console.log(temperatureData);
+
+//             });
+
+//             //weatherTxt += '</div>';
+
+//             returnData.push(returnItem);
+
+//         }
+//         console.log(city);
+
+//         //return returnData;
+//         appendData(returnData);
+//     }
+
+
+// }
 
 function error(err) {
     console.log('Unable to fetch weather data, please check your internet connection and try again');
 }
 
 function appendData(inputData) {
-    console.log(inputData);
+    console.log('name', inputData.name);
+    // console.log('humidity', inputData.temperature.humidity);
+    console.log('weather', inputData.weather);
 
 }
-
-
-(function() {
-
-    'use strict';
-
-    getData('http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=07c942ac99d05229807385fde0045886')
-        .then(processWeatherData(weather))
-        .catch(error);
-
-})();
-
